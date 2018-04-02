c8396dee461a3dd0bc7350edef674881a12d1a7d
Mariya Zagainova <mzagainova@nevada.unr.edu>
2018-04-02 12:40:56 -0700

    Add files via upload

--- src/Command.cpp
+++ src/Command.cpp
@@ -6,11 +6,6 @@
  */
 #include <Command.h>
 
-Command::Command()
-{
-
-}
-
 Command::Command(Entity381* ent, COMMAND_TYPE ct){
 
 }
@@ -28,49 +23,35 @@ void Command::tick(float dt){
 }
 
 bool Command::done(){
-	return true;
+
 }
 
 
-MoveTo::MoveTo(Entity381* ent, Ogre::Vector3 location)
-{
-	targetLocation = location;
-	entity = ent;
-	MOVE_DISTANCE_THRESHOLD = targetLocation.squaredDistance(entity->position);
-	init();
+MoveTo::MoveTo(Entity381* ent, Ogre::Vector3 location):
+	Command(ent, MoveTo){
+
 }
 
-MoveTo::~MoveTo(){
+MoveTo::MoveTo~MoveTo(){
 
 }
 
 void MoveTo::init(){
-	Ogre::Vector3 difference = targetLocation - entity->position;
-	Ogre::Radian temp = Ogre::Math::ATan2(difference.y, difference.x) * 180/ 3.1415926;
-	entity->desiredHeading = temp.valueRadians();
-	entity->desiredSpeed = entity->maxSpeed;
+
 }
 
 // move currently selected ent to location
 void MoveTo::tick(float dt){
-	MOVE_DISTANCE_THRESHOLD = targetLocation.squaredDistance(entity->position);
 
-	if(MOVE_DISTANCE_THRESHOLD < 100)
-	{
-		entity->desiredSpeed = 0;
-	}
 }
 
 bool MoveTo::done(){
-	if(MOVE_DISTANCE_THRESHOLD < 10)
-	{
-		return true;
-	}
-	return false;
+
 }
 
 
-Intercept::Intercept(Entity381* ent, Ogre::Vector3 location){
+Intercept::Intercept(Entity381* ent, Ogre::Vector3 location):
+	Command(ent, Intercept){
 
 }
 
